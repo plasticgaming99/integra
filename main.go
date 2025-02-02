c7f5c6abfca01534dd060b906326128f77580f4c
plasticgaming99 <plsplassub@gmail.com>
2025-02-02 13:09:57 +0900

    Loop for package

--- main.go
+++ main.go
@@ -77,108 +77,11 @@ func main() {
 		printdbg(localtrdb[i][0][0])
 	}
 
-	if install {
-		var packagename string
-		var intgpack archives.Tar
-		abs, err := filepath.Abs(pack2ins[0])
-		if err != nil {
-			fmt.Println(err)
-		}
-		in, err := os.Open(abs)
-		if err != nil {
-			fmt.Println("Error opening archive file")
-		}
-		defer in.Close()
-		input := bufio.NewReader(in)
-
-		getPackName := func(ctx context.Context, f archives.FileInfo) error {
-			if f.NameInArchive == ".PACKAGE" {
-				fssss, err := f.Open()
-				if err != nil {
-					return err
-				}
-				defer fssss.Close()
-				toreadbyte, err := io.ReadAll(fssss)
-				if err != nil {
-					return err
-				}
-				stringToOperate := string(toreadbyte)
-				slice2op := strings.Split(stringToOperate, "\n")
-				for i := 0; i < len(slice2op); i++ {
-					if s := strings.Split(slice2op[i], " = "); s[0] == "package" {
-						packagename = s[1]
-					}
-				}
-			}
-			if f.IsDir() {
-				return nil
-			}
-			return nil
-		}
-
-		installArchive := func(ctx context.Context, f archives.FileInfo) error {
-			printdbg(f.Name())
-			destpath := rootdir + f.NameInArchive
-			if f.IsDir() {
-				err := os.MkdirAll(destpath, f.Mode().Perm())
-				printdbg(err)
-				return err
-			}
-
-			if f.LinkTarget != "" {
-				targAbs, err := filepath.Abs(filepath.Join(rootdir, f.LinkTarget))
-				printdbg("symlink target:", targAbs)
-				if err != nil {
-					printdbg(err)
-					return err
-				}
-				nameinarchiveABS, err := filepath.Abs(filepath.Join(rootdir, f.NameInArchive))
-				if err != nil {
-					printdbg(err)
-					return err
-				}
-				err = os.Symlink(targAbs, nameinarchiveABS)
-				printdbg(err)
-				return err
-			}
-
-			reader, err := f.Open()
-			if err != nil {
-				printdbg(err)
-				return err
-			}
-			bufread := bufio.NewReader(reader)
-			defer reader.Close()
-
-			if f.Name() == ".PACKAGE" || f.Name() == ".MTREE" {
-				destpath = filepath.Join(dbdir, "local", packagename, f.NameInArchive)
-				mdAllIfNeeded(filepath.Join(dbdir, "local", packagename))
-				dest, err := os.OpenFile(destpath, os.O_CREATE|os.O_WRONLY, f.Mode())
-				if err != nil {
-					log.Fatal("error writing db")
-				}
-				io.Copy(dest, bufread)
-				return nil
-			}
-
-			destfile, err := os.OpenFile(destpath, os.O_CREATE|os.O_WRONLY, f.Mode().Perm())
-			if err != nil {
-				printdbg(err)
-				return err
-			}
-			bufdest := bufio.NewWriter(destfile)
-			defer destfile.Close()
-
-			_, err = io.Copy(bufdest, bufread)
-			if err != nil {
-				printdbg(err)
-				return err
-			}
-			return nil
-		}
-
-		{
-			abs, err := filepath.Abs(pack2ins[0])
+	for cnt := 1; cnt < len(pack2ins); cnt++ {
+		if install {
+			var packagename string
+			var intgpack archives.Tar
+			abs, err := filepath.Abs(pack2ins[cnt])
 			if err != nil {
 				fmt.Println(err)
 			}
@@ -187,38 +90,137 @@ func main() {
 				fmt.Println("Error opening archive file")
 			}
 			defer in.Close()
-			tInput := bufio.NewReader(in)
+			input := bufio.NewReader(in)
 
-			intgpack.Extract(context.Background(), tInput, getPackName)
+			getPackName := func(ctx context.Context, f archives.FileInfo) error {
+				if f.NameInArchive == ".PACKAGE" {
+					fssss, err := f.Open()
+					if err != nil {
+						return err
+					}
+					defer fssss.Close()
+					toreadbyte, err := io.ReadAll(fssss)
+					if err != nil {
+						return err
+					}
+					stringToOperate := string(toreadbyte)
+					slice2op := strings.Split(stringToOperate, "\n")
+					for i := 0; i < len(slice2op); i++ {
+						if s := strings.Split(slice2op[i], " = "); s[0] == "package" {
+							packagename = s[1]
+						}
+					}
+				}
+				if f.IsDir() {
+					return nil
+				}
+				return nil
+			}
+
+			installArchive := func(ctx context.Context, f archives.FileInfo) error {
+				printdbg(f.Name())
+				destpath := rootdir + f.NameInArchive
+				if f.IsDir() {
+					err := os.MkdirAll(destpath, f.Mode().Perm())
+					printdbg(err)
+					return err
+				}
+
+				if f.LinkTarget != "" {
+					targAbs, err := filepath.Abs(filepath.Join(rootdir, f.LinkTarget))
+					printdbg("symlink target:", targAbs)
+					if err != nil {
+						printdbg(err)
+						return err
+					}
+					nameinarchiveABS, err := filepath.Abs(filepath.Join(rootdir, f.NameInArchive))
+					if err != nil {
+						printdbg(err)
+						return err
+					}
+					err = os.Symlink(targAbs, nameinarchiveABS)
+					printdbg(err)
+					return err
+				}
+
+				reader, err := f.Open()
+				if err != nil {
+					printdbg(err)
+					return err
+				}
+				bufread := bufio.NewReader(reader)
+				defer reader.Close()
+
+				if f.Name() == ".PACKAGE" || f.Name() == ".MTREE" {
+					destpath = filepath.Join(dbdir, "local", packagename, f.NameInArchive)
+					mdAllIfNeeded(filepath.Join(dbdir, "local", packagename))
+					dest, err := os.OpenFile(destpath, os.O_CREATE|os.O_WRONLY, f.Mode())
+					if err != nil {
+						log.Fatal("error writing db")
+					}
+					io.Copy(dest, bufread)
+					return nil
+				}
+
+				destfile, err := os.OpenFile(destpath, os.O_CREATE|os.O_WRONLY, f.Mode().Perm())
+				if err != nil {
+					printdbg(err)
+					return err
+				}
+				bufdest := bufio.NewWriter(destfile)
+				defer destfile.Close()
+
+				_, err = io.Copy(bufdest, bufread)
+				if err != nil {
+					printdbg(err)
+					return err
+				}
+				return nil
+			}
+
+			{
+				abs, err := filepath.Abs(pack2ins[cnt])
+				if err != nil {
+					fmt.Println(err)
+				}
+				in, err := os.Open(abs)
+				if err != nil {
+					fmt.Println("Error opening archive file")
+				}
+				defer in.Close()
+				tInput := bufio.NewReader(in)
+
+				intgpack.Extract(context.Background(), tInput, getPackName)
+			}
+
+			intgpack.Extract(context.Background(), input, installArchive)
+			// err := intgpack.Extract(nil, nil, nil)
 		}
 
-		intgpack.Extract(context.Background(), input, installArchive)
-		// err := intgpack.Extract(nil, nil, nil)
-	}
-
-	if remove {
-		printdbg("start remove")
-		var inttt int
-		printdbg("toremove:", pack2ins[0])
-		for i := 0; i < len(pack2ins); i++ {
-			if localtrdb[0][0][0] == pack2ins[0] {
-				printdbg("yes")
-				inttt = i
+		if remove {
+			printdbg("start remove")
+			var inttt int
+			printdbg("toremove:", pack2ins[cnt])
+			for i := 0; i < len(pack2ins); i++ {
+				if localtrdb[i][0][0] == pack2ins[cnt] {
+					printdbg("yes")
+					inttt = i
+				}
 			}
+			printdbg("db id:", inttt)
+			for i := 2; i < len(localtrdb[inttt])-2; i++ {
+				path2remove := localtrdb[inttt][i][0]
+				path2remove, err := filepath.Abs(filepath.Join(rootdir, path2remove))
+				if err != nil {
+					log.Fatal("error during removing")
+				}
+				if !(strings.HasPrefix(path2remove, "/set") || strings.HasPrefix(path2remove, "./.PACKAGE")) {
+					printdbg(path2remove)
+					os.Remove(path2remove)
+				}
+			}
+			os.RemoveAll(filepath.Join(dbdir, pack2ins[cnt]))
 		}
-		printdbg("db id:", inttt)
-		for i := 2; i < len(localtrdb[inttt])-2; i++ {
-			path2remove := localtrdb[inttt][i][0]
-			path2remove, err := filepath.Abs(filepath.Join(rootdir, path2remove))
-			if err != nil {
-				log.Fatal("error during removing")
-			}
-			if !(strings.HasPrefix(path2remove, "/set") || strings.HasPrefix(path2remove, "./.PACKAGE")) {
-				printdbg(path2remove)
-				os.Remove(path2remove)
-			}
-		}
-		os.RemoveAll(filepath.Join(dbdir, pack2ins[0]))
 	}
 }
 
